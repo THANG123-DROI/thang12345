758a650e7cad509454b4e961ec9a47fc8645757a
Thang Nguyen <thangndph10927@fpt.edu.vn>
2021-08-07 15:24:37 +0700

    initial commit

---
+++ thangndph10927-ass/src/app/data-fake.ts
@@ -0,0 +1,17 @@
+import { ProductType } from './product/product';
+export const dataFake: ProductType[] = [
+    {
+        id: 1,
+        name: "Product 1",
+        price: 20,
+        status: false,
+        img: "https://salt.tikicdn.com/cache/280x280/ts/product/52/db/ed/250ed1f223e0711ac9f3ea93ade7074d.jpg"
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        price: 30,
+        status: false,
+        img: "https://salt.tikicdn.com/cache/280x280/ts/product/52/db/ed/250ed1f223e0711ac9f3ea93ade7074d.jpg"
+    },
+];
